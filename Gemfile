9f2a59bd0d62f86ecfc8923b56d7d085bdafdfc3
Ben Conway <bendavidconway@me.com>
2014-12-08 16:31:34 +0000

    mailgun working

--- Gemfile
+++ Gemfile
@@ -1,6 +1,8 @@
 source 'https://rubygems.org'
 
 gem 'sinatra'
+gem 'data_mapper'
+gem 'dm-postgres-adapter'
 
 group :development, :test do
   gem 'capybara'
--- lib/game.rb
+++ lib/game.rb
@@ -8,7 +8,7 @@ class Game
   end
 
   def winner(players_choice, computers_choice)
-    win_combos = {scissors: :paper, rock: :scissors, paper: :rock}
+    win_combos = {scissors: :paper, scissors: :lizard, rock: :scissors, paper: :rock}
     return "Draw" if players_choice == computers_choice
     return "Player wins" if win_combos[players_choice] == computers_choice
     return "Computer wins"
